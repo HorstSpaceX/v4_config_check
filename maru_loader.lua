f209fe98847a4fc1e772ee30c72f2c599411ef02
HorstSpaceX <maxgtlove2@gmail.com>
2025-05-16 15:24:35 +0700

    Update maru_loader.lua

--- maru_loader.lua
+++ maru_loader.lua
@@ -1,49 +1,42 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 
-local Players = game:GetService("Players")
-local localName = Players.LocalPlayer.Name
+task.spawn(function()
+    local Players = game:GetService("Players")
+    local localName = Players.LocalPlayer.Name
 
--- Merge Username To Easy to Find
-local allowedNames = {}
+    -- Merge Username To Easy to Find
+    local allowedNames = {}
 
--- รวม HelperNameList
-for _, name in ipairs(_G.Settings_V4["HelperNameList"]) do
-    allowedNames[name] = true
-end
-
--- รวม V4FarmList
-for _, name in ipairs(_G.Settings_V4["V4FarmList"]) do
-    allowedNames[name] = true
-end
-
-if not allowedNames[localName] then
-    --print("Nt Found Username")
-    return
-end
-
-
-function HasMain()
-    local gui = game.Players.LocalPlayer:FindFirstChild("PlayerGui")
-    if gui and gui:FindFirstChild("Main (minimal)") then
-        print("Found")
-        --return true
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MasterPClient/main/Loader.lua"))()
+    -- รวม HelperNameList
+    for _, name in ipairs(_G.Settings_V4["HelperNameList"]) do
+        allowedNames[name] = true
     end
-    print("Not Found")
-end
 
+    -- รวม V4FarmList
+    for _, name in ipairs(_G.Settings_V4["V4FarmList"]) do
+        allowedNames[name] = true
+    end
 
-while true do
-    task.spawn(function()
+    if not allowedNames[localName] then
+        return
+    end
+
+    function HasMain()
+        local gui = game.Players.LocalPlayer:FindFirstChild("PlayerGui")
+        if gui and gui:FindFirstChild("Main (minimal)") then
+            print("Found")
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MasterPClient/main/Loader.lua"))()
+        end
+        print("Not Found")
+    end
+
+    while true do
         local success, err = pcall(function()
             HasMain()
         end)
         if not success then
             warn("Error in HasMain:", err)
         end
-    end)
-    task.wait(21)
-end
-
-
-    
+        task.wait(21)
+    end
+end)
